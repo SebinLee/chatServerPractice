789ef1a57e3820505c6ff41da05c153f916f9c85
SebinLee <sebinlee.artdev@gmail.com>
2020-10-31 20:35:17 +0900

    Add mongoConnection function

---
+++ server/utils/mongoConnection.ts
@@ -0,0 +1,15 @@
+import { mongoose } from "@typegoose/typegoose";
+require("dotenv").config("./env");
+
+/**
+ * @description Function that provides MongoDB Connection
+ */
+export default async function mongoConnection() {
+    const CONNECTION_URL = `mongodb+srv://${process.env.MONGO_URL}/${process.env.MONGO_NAME}`;
+
+    // Make a mongo connection and return it
+    return mongoose.connect(CONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+}
